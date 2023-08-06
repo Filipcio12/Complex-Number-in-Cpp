824e6a93efef6bba9b3671b549de6500576e73f9
Filip <filipfajfer5@gmail.com>
2023-08-06 20:48:31 +0200

    Feat tests

--- TestComplex.cpp
+++ TestComplex.cpp
@@ -2,7 +2,24 @@
 
 int main()
 {
-    ComplexNumber num(1, 0);
-    std::cout << (1 != num) << std::endl;
+    ComplexNumber a(1, 1);
+    ComplexNumber b(2, 1);
+    ComplexNumber c(1, 2);
+
+    //a += b += c;
+    //(a += b) += c;
+    //a *= b *= c;
+    //(a *= b) *= c;
+
+    /*std::cout << "a: " << a << "\n";
+    std::cout << "b: " << b << "\n";
+    std::cout << "c: " << c << "\n";*/
+
+    std::cout << "a + 7 = " << a + 7 << "\n";
+    std::cout << "7 + a = " << 7 + a << "\n";
+    std::cout << "b * 5 = " << b * 5 << "\n";
+    std::cout << "5 * b = " << 5 * b << "\n";
+    std::cout << "c == 3.5 = " << (c == 3.5) << "\n";
+    std::cout << "3.5 == c = " << (3.5 == c) << "\n";
     return 0;
 }
